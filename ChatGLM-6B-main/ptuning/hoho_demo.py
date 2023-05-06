549a07e9969a96fed55a6fce9d9f2e98690ad9e1
hoho <kevinvalentine@outlook.com>
2023-05-06 10:17:13 +0800

    添加可选参数输入

--- ChatGLM-6B-main/ptuning/hoho_demo.py
+++ ChatGLM-6B-main/ptuning/hoho_demo.py
@@ -1,29 +1,38 @@
 import os
 import argparse
 import torch
-from transformers import AutoConfig, AutoModel, AutoTokenizer 
+from transformers import AutoConfig, AutoModel, AutoTokenizer
 
 
 CHECKPOINT_PATH = "output/adgen-chatglm-6b-int4-pt-128-2e-2/checkpoint-3000"
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code = True)
-config = AutoConfig.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code = True, pre_seq_len = 128)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", config = config, trust_remote_code = True)
-prefix_state_dict = torch.load(f"{CHECKPOINT_PATH}/pytorch_model.bin")
+g_parser = argparse.ArgumentParser(description = "Help Information")
+g_parser.add_argument('--query', '-q', default = '你好', help = 'input your question')
+g_args = g_parser.parse_args()
 
-# print(f"hoho: prefix_state_dict: {prefix_state_dict}")
+def main():
+    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code = True)
+    config = AutoConfig.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code = True, pre_seq_len = 128)
+    model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", config = config, trust_remote_code = True)
+    prefix_state_dict = torch.load(f"{CHECKPOINT_PATH}/pytorch_model.bin")
 
-new_prefix_state_dict = {}
-for k, v in prefix_state_dict.items():
-    if k.startswith("transformer.prefix_encoder."):
-        new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v  # 提取“transformer.prefix_encoder.”后面剩余的字符串作为key
-model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
+    # print(f"hoho: prefix_state_dict: {prefix_state_dict}")
+
+    new_prefix_state_dict = {}
+    for k, v in prefix_state_dict.items():
+        if k.startswith("transformer.prefix_encoder."):
+            new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v  # 提取“transformer.prefix_encoder.”后面剩余的字符串作为key
+    model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
 
 
-# model = model.quantize(4) # 已经是int4了，不需要再quantize了
-model= model.half().cuda()
-model.transformer.prefix_encoder.float()
-model = model.eval()
+    # model = model.quantize(4) # 已经是int4了，不需要再quantize了
+    model= model.half().cuda()
+    model.transformer.prefix_encoder.float()
+    model = model.eval()
 
-response, history = model.chat(tokenizer, "你好", history = [])
-print(f'response: {response}\n history: {history}')
+    response, history = model.chat(tokenizer, g_args.query, history = [])
+    print(f'response: {response}\n history: {history}')
+
+
+if __name__ == "__main__":
+    main()
