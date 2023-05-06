cd497bea1bd13e50a331ff786fe12af882beac6e
hoho <kevinvalentine@outlook.com>
2023-05-06 09:20:52 +0800

    删除多余代码

--- ChatGLM-6B-main/ptuning/hoho_demo.py
+++ ChatGLM-6B-main/ptuning/hoho_demo.py
@@ -11,17 +11,17 @@ prefix_state_dict = torch.load(f"{CHECKPOINT_PATH}/pytorch_model.bin")
 
 print(f"hoho: prefix_state_dict: {prefix_state_dict}")
 
-# new_prefix_state_dict = {}
-# for k, v in prefix_state_dict.items():
-#     if k.startswith("transformer.prefix_encoder."):
-#         new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v
-# model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
+new_prefix_state_dict = {}
+for k, v in prefix_state_dict.items():
+    if k.startswith("transformer.prefix_encoder."):
+        new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v  # 提取“transformer.prefix_encoder.”后面剩余的字符串作为key
+model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
 
 
-# # model = model.quantize(4) # 已经是int4了，不需要再quantize了？
-# model= model.half().cuda()
-# model.transformer.prefix_encoder.float()
-# model = model.eval()
+# model = model.quantize(4) # 已经是int4了，不需要再quantize了
+model= model.half().cuda()
+model.transformer.prefix_encoder.float()
+model = model.eval()
 
-# response, history = model.chat(tokenizer, "你好", history = [])
-# print(f'response: {response}\n history: {history}')
+response, history = model.chat(tokenizer, "你好", history = [])
+print(f'response: {response}\n history: {history}')
