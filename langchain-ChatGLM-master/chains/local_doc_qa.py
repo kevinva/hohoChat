234079bb8cddb1f25c0f3c2234209e45712f36f6
hoho <kevinvalentine@outlook.com>
2023-05-30 16:31:59 +0800

    添加注释

--- langchain-ChatGLM-master/chains/local_doc_qa.py
+++ langchain-ChatGLM-master/chains/local_doc_qa.py
@@ -41,6 +41,12 @@ def generate_prompt(related_docs: List[str],
                     prompt_template=PROMPT_TEMPLATE) -> str:
     context = "\n".join([doc.page_content for doc in related_docs])
     prompt = prompt_template.replace("{question}", query).replace("{context}", context)
+
+
+    print(f'hoho: prompt: {prompt} \n len docs: {len(related_docs)}')
+    for docs in related_docs:
+        print(f"hoho: docs's len: {docs.page_content.__len__()}")
+
     return prompt
 
 
