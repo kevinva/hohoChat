a352fed7a17a3a400e116520567691a5411e29bc
hezhi <kevinvalentine@outlook.com>
2023-06-28 21:17:32 +0800

    test

--- dev/hoho_demo.py
+++ dev/hoho_demo.py
@@ -5,29 +5,36 @@ from transformers import AutoConfig, AutoModel, AutoTokenizer
 
 
 CHECKPOINT_PATH = "output/law-chatglm-6b-int4-pt-128-2e-2/checkpoint-3000"
+PTUNING_ENABLE = False
+MODEL_PATH = r"H:\AI\model\chatglm-6b"
+MODEL_NAME = "THUDM/chatglm-6b-int4"
+
+DEVICE = "cuda:1" if torch.cuda.is_available() else "cpu"
 
 g_parser = argparse.ArgumentParser(description = "Help Information")
 g_parser.add_argument('--query', '-q', default = '你好', help = 'input your question')
 g_args = g_parser.parse_args()
 
 def main():
-    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code = True, revision = "v0.1.0")
-    config = AutoConfig.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code = True, pre_seq_len = 128, revision = "v0.1.0")
-    model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", config = config, trust_remote_code = True, revision = "v0.1.0")
-    prefix_state_dict = torch.load(f"{CHECKPOINT_PATH}/pytorch_model.bin")
-
-    # print(f"hoho: prefix_state_dict: {prefix_state_dict}")
     print(f"query = {g_args.query}")
 
-    new_prefix_state_dict = {}
-    for k, v in prefix_state_dict.items():
-        if k.startswith("transformer.prefix_encoder."):
-            new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v  # 提取“transformer.prefix_encoder.”后面剩余的字符串作为key
-    model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, trust_remote_code = True, revision = "v0.1.0")
+    config = AutoConfig.from_pretrained(MODEL_PATH, trust_remote_code = True, pre_seq_len = 128, revision = "v0.1.0")
+    model = AutoModel.from_pretrained(MODEL_PATH, config = config, trust_remote_code = True, revision = "v0.1.0")
 
+    if PTUNING_ENABLE:
+        prefix_state_dict = torch.load(f"{CHECKPOINT_PATH}/pytorch_model.bin")
+
+        # print(f"hoho: prefix_state_dict: {prefix_state_dict}")
+
+        new_prefix_state_dict = {}
+        for k, v in prefix_state_dict.items():
+            if k.startswith("transformer.prefix_encoder."):
+                new_prefix_state_dict[k[len("transformer.prefix_encoder."):]] = v  # 提取“transformer.prefix_encoder.”后面剩余的字符串作为key
+        model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
 
     # model = model.quantize(4) # 已经是int4了，不需要再quantize了
-    model= model.half().cuda()
+    model= model.half().to(DEVICE)
     model.transformer.prefix_encoder.float()
     model = model.eval()
 
@@ -36,5 +43,5 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-    print(f'main: cuda = {torch.cuda.is_available()}, cudnn = {torch.backends.cudnn.enabled}')
+    main()
+    # print(f'main: cuda = {torch.cuda.is_available()}, cudnn = {torch.backends.cudnn.enabled}')
