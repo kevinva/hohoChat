7e10bb21d2371806a445a7e856e6aa3a0bf6b340
hoho <kevinvalentine@outlook.com>
2023-06-29 15:18:50 +0800

    test

--- dev/hoho_canton_economic.py
+++ dev/hoho_canton_economic.py
@@ -1,9 +1,10 @@
 import time
 import os, sys
 import json
+import re
 
 # 放在所有访问GPU代码之前（作为从卡，window要设置这个才行）
-os.environ['CUDA_VISIBLE_DEVICES']='1' 
+# os.environ['CUDA_VISIBLE_DEVICES']='1' 
 # os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:128"  # window要设置这个，否则会即使显存够也会出现OOM错误
 
 langchain_ChatGLM_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "langchain-ChatGLM-master")
@@ -19,26 +20,25 @@ from langchain.document_loaders import UnstructuredPDFLoader, DirectoryLoader, T
 from langchain.schema import Document
 
 
-# from langchain-ChatGLM-master
+# langchain-ChatGLM-master模块
 from configs import model_config
 from models.chatglm_llm import ChatGLM
 from chains.local_doc_qa import similarity_search_with_score_by_vector, LLM_HISTORY_LEN, get_docs_with_score
 
-# from tools
+# tools模块
 from hoho_utils import logTime, strForTime_YmdHMS
 from hoho_huggingface import HohoHuggingFaceEmbeddings
 
 
 LOG_PREFIX = "[CAN_ECONOMIC]"
-DOCS_DATA_PATH = r"F:\AI\hohoChat\data\can_economic.pdf"
-LLM_MODEL_PATH = r"H:\AI\LLMs\chatglm2-6b-int4"
-EMBEDDING_MODEL_PATH = r"H:\AI\embedding_models\multi-qa-mpnet-base-dot-v1"
+DOCS_DATA_PATH = "../data/test_doc.pdf"
+LLM_MODEL_PATH = "../models/chatglm2-6b-int4"
+EMBEDDING_MODEL_PATH = "../models/multi-qa-mpnet-base-dot-v1"
+
 PROMPT_TEMPLATE = """
-你是一名数据分析师，请基于你的知识从以下文本中提取指标名称、对应的指标值、数值单位和数据期，结果以json形式返回。
-如果结果有多个，则以jsonlines形式返回。
-如果你对答案不确定，请说 “我不知道”。
-文本：{content}
-结果：
+###{content}###
+
+从上文中，提取"指标名称"、"指标值"、"数值单位"、"数据期"等类型的实体，以json列表形式输出提取结果。
 """
 
 
@@ -47,25 +47,27 @@ def init_knowledge_based(docs_path = None, embedding_model_path = None):
 
     if embedding_model_path is not None:
         embedding_model_name = os.path.basename(embedding_model_path)
-        embeddings = HohoHuggingFaceEmbeddings(model_name = embedding_model_path, 
+        embeddings = HohoHuggingFaceEmbeddings(model_name = embedding_model_path,
                                                cache_folder = embedding_model_path,
                                                model_kwargs = {'device': model_config.EMBEDDING_DEVICE})
     else:
         embedding_model_name = model_config.EMBEDDING_MODEL
-        embeddings = HuggingFaceEmbeddings(model_name = model_config.embedding_model_dict[model_config.EMBEDDING_MODEL], 
+        embeddings = HuggingFaceEmbeddings(model_name = model_config.embedding_model_dict[model_config.EMBEDDING_MODEL],
                                            model_kwargs = {'device': model_config.EMBEDDING_DEVICE})
-    
+
     if docs_path is None:
         print(f"{LOG_PREFIX}[{logTime()}] docs_path is None!")
         return None
 
-    text_splitter = TokenTextSplitter(chunk_size = 250 , chunk_overlap = 0, disallowed_special = ())
-    docs = []
     loader = UnstructuredPDFLoader(docs_path)
-    print(f"loader: {loader}")
+    data = loader.load()
+    print(f"{LOG_PREFIX}[{logTime()}] Document's len: {len(data[0].page_content)}")
+
+    text_splitter = TokenTextSplitter(chunk_size = 2000, chunk_overlap = 0, disallowed_special = ())
+    docs = []
     docs += loader.load_and_split(text_splitter)
 
-    print(f"{LOG_PREFIX}[{logTime()}] Number of docs: {len(docs)}")
+    print(f"{LOG_PREFIX}[{logTime()}] Number of chunks: {len(docs)}")
     print(f"{LOG_PREFIX}[{logTime}] Load knowledge done! Elapsed time: {time.time() - start_time} seconds")
 
     return docs
@@ -100,25 +102,41 @@ g_llm = init_llm(local_path = LLM_MODEL_PATH)
 print(f"{LOG_PREFIX}[{logTime()}] Initializing llm successfully!", flush = True)
 
 
-def get_answer(content):
+
+def contains_arabic_numbers(text):
+    pattern = r"\d"
+    match = re.search(pattern, text)
+    return match is not None
+
+
+def get_answer(query):
     start_time = time.time()
 
-    prompt = PROMPT_TEMPLATE.format(content = content)
-    print(f'{LOG_PREFIX} [get_answer] prompt: {prompt}')
+    prompt = PROMPT_TEMPLATE.format(content = query)
 
     final_result = None
     for result, history in g_llm._call(prompt = prompt, history = [], streaming = False):
         final_result = result
 
+    print(f"{LOG_PREFIX}[{logTime()}] Elapsed time: {time.time() - start_time} seconds")
+
     return final_result
 
 
 def main():
+    result_list = []
     for i, doc in enumerate(g_docs):
-        print(doc)
-        if i > 10:
-            break
-
+        print(f"###### doc {i} ######")
+        if contains_arabic_numbers(doc.page_content):
+            result_list.append(get_answer(doc.page_content))
+        else:
+            print("skip!")
+    
+    output_file = "../outputs/result_json.txt"
+    result_str = "\n\n\n\n\n\n".join(result_list)
+    with open(output_file, "w", encoding = "utf-8") as f:
+        f.write(result_str)
+    
 
 if __name__ == "__main__":
     main()
