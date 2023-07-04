beddbdf60fcdf35fd2745847284bbea2d66a35e3
hoho <kevinvalentine@outlook.com>
2023-07-04 13:29:58 +0800

    test

--- dev/demo_chat_shuiju/GL_topic_clustering_v2.py
+++ dev/demo_chat_shuiju/GL_topic_clustering_v2.py
@@ -6,13 +6,10 @@ from langchain.text_splitter import MarkdownTextSplitter, CharacterTextSplitter 
 import pandas as pd
 import torch
 
-from sklearn.metrics import davies_bouldin_score
-
 EMBEDDING_DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-EMBEDDING_PATH = r"C:\Users\hoho\Documents\WXWork\1688854731468464\Cache\File\2023-07\sentence_pair_sim"
 
 # 用FAISS.from_documents 去加载文本
-topic_data = pd.read_excel(r'outputs/对话主题总结_20230619213456.xlsx')
+topic_data = pd.read_excel(r'duihua_xlsx/对话主题总结_20230619213456.xlsx')
 print('未删除空值前', topic_data.shape[0])
 topic_data = topic_data.dropna()
 print('删除空值后', topic_data.shape[0])
@@ -41,13 +38,11 @@ topic = [t.replace('主题：','').replace('客户关心的','')
 # 加载模型，将数据进行向量化处理
 from sentence_transformers import SentenceTransformer, util
 import numpy as np
-model_name = 'hfl/chinese-roberta-wwm-ext'
-model = SentenceTransformer(EMBEDDING_PATH)
+model_name = 'sent_model/sentence_pair_sim/' #'hfl/chinese-roberta-wwm-ext'
+model = SentenceTransformer(model_name)
 #sent_model/sentence_pair_sim/   hfl/chinese-roberta-wwm-ext
 
 sentence_embeddings = model.encode(topic)
-
-print(f"sentence_embeddings: {sentence_embeddings.shape}")
 
 #采用AffinityPropagation 层次聚类
 # from sklearn.cluster import AffinityPropagation
@@ -71,48 +66,46 @@ class UnionFind():
     def merge(self, x, y):
         self.union[self.find(x)] = self.find(y)
 
-# #计算两两相似度
-# similarity_matrix = util.pytorch_cos_sim(sentence_embeddings, sentence_embeddings) # 计算余弦相似度矩阵
-# similarity_matrix = similarity_matrix.to('cpu').numpy()
+#计算两两相似度
+similarity_matrix = util.pytorch_cos_sim(sentence_embeddings, sentence_embeddings) # 计算余弦相似度矩阵
+similarity_matrix = similarity_matrix.to('cpu').numpy()
 
 
-# mask = 1 - np.eye(similarity_matrix.shape[0]) # 使用掩码将对角线元素清0
-# similarity_matrix = similarity_matrix * mask
-# max_similarity = similarity_matrix.max(axis=1) # 每个词与其他所有词的最大相似度
-# max_index = np.argmax(similarity_matrix, axis=1) # 每个词与其最相似的下标
+mask = 1 - np.eye(similarity_matrix.shape[0]) # 使用掩码将对角线元素清0
+similarity_matrix = similarity_matrix * mask
+max_similarity = similarity_matrix.max(axis=1) # 每个词与其他所有词的最大相似度
+max_index = np.argmax(similarity_matrix, axis=1) # 每个词与其最相似的下标
 
-# # 合并相似度大于等于阈值
-# threshold = 0.85
-# dsu = UnionFind(sentence_embeddings.shape[0])
-# for i in range(sentence_embeddings.shape[0]):
-#     if max_similarity[i] >= threshold:
-#         dsu.merge(i, max_index[i])
+# 合并相似度大于等于阈值
+threshold = 0.87
+dsu = UnionFind(sentence_embeddings.shape[0])
+for i in range(sentence_embeddings.shape[0]):
+    if max_similarity[i] >= threshold:
+        dsu.merge(i, max_index[i])
 
-# _topic_group = {}
-# for i in range(sentence_embeddings.shape[0]):
-#     if dsu.find(i) not in _topic_group:
-#         _topic_group[dsu.find(i)] = []
-#     _topic_group[dsu.find(i)].append(topic[i])
+_topic_group = {}
+for i in range(sentence_embeddings.shape[0]):
+    if dsu.find(i) not in _topic_group:
+        _topic_group[dsu.find(i)] = []
+    _topic_group[dsu.find(i)].append(topic[i])
 
-# dict_topic = {}
-# for k,v in _topic_group.items():
-#     for i in v:
-#         dict_topic[i] = k
-
-
-# # labels = [dict_topic[k] for k in topic]
-# # db_index = davies_bouldin_score(sentence_embeddings, labels)
-
-# # print(f'sentence_embeddings: {sentence_embeddings.shape}')
-# # print(f'labels: {len(labels)}')
-# # print(f'DBI: {db_index}')
-
-# topic_data['主题分类'] = [dict_topic[k] for k in topic]
-# topic_data['最终主题'] = topic_data.groupby('主题分类')['主题'].transform(lambda x: x.iloc[x.str.len().argmin()])
-# topic_data.to_excel(r'outputs/主题分类-{}-{}.xlsx'.format(model_name.replace('/','_')
-#                                                        ,threshold))
+dict_topic = {}
+for k,v in _topic_group.items():
+    for i in v:
+        dict_topic[i] = k
 
 
+topic_data['主题分类'] = [dict_topic[k] for k in topic]
+topic_data['主题'] = topic
+topic_data['主题长度'] = topic_data['主题'].apply(lambda x:len(x))
+topic_data = topic_data[topic_data['主题长度'] <= 30]
+topic_data['原对话'].apply(lambda x:x.replace(
+    '：请基于以下客服与客户对话，进行总结任务——客户关心的主题，总结字数在10个字以内，返回格式：主题：对应主题 对话如下：""""',
+    ''))
 
+#topic_data[['主题', '主题长度']].groupby(['主题长度']).min()
+topic_data['分类后主题'] = topic_data.groupby('主题分类')['主题'].transform(lambda x: x.iloc[x.str.len().argmin()])
+topic_data.to_excel(r'output_xlsx/主题分类-{}-{}.xlsx'.format(model_name.replace('/','_')
+                                                       ,threshold))
 
 
